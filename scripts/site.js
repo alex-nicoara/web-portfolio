412dd9feafbdbcc1823534d6b3f78ea3c497383c
Alex Nicoara <anicoara@hawk.iit.edu>
2018-05-01 14:21:16 -0500

    Scrolling Nav works

--- scripts/site.js
+++ scripts/site.js
@@ -1,5 +1,29 @@
 // 'load' event
 $(function(){
+  // Trigger Menu on Click
   $("#menu-btn").click(function(){
+    if($('.hslide').hasClass('animate')) {
+      $('.hslide').removeClass('animate');
+    }else{
+      $('.hslide').addClass('animate');
+    }
   });
+
+  // Back-to-top Button
+  window.onscroll = function() {scrollFunction()};
+  $('#back-to-top').click(goToTop);
+
+  // Functions
+  function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          $('#back-to-top').css('display','block');
+      } else {
+          $('#back-to-top').css('display','none');
+      }
+  }
+
+  function goToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
 });
